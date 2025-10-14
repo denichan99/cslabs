f111f96392eba0bdf2cd2e8f2dd35a25516584a6
denichan99 <103909817+denichan99@users.noreply.github.com>
2025-10-14 20:00:58 +0500

    Add files via upload

---
+++ lab 1/Methods.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace cs_lab1_main
+{
+    internal class Methods
+    {
+        public double fraction(double x)
+        {
+            return x - Math.Floor(x);
+        }
+
+        public int charToNum(char x)
+        {
+            return x - '0';
+        }
+
+        public bool is2Digits(int x)
+        {
+            return x > 9 && x < 100;
+        }
+
+        public bool isInRange(int a, int b, int num)
+        {
+            int max = Math.Max(a, b);
+            int min = (a + b) - max;
+            return num > min && num < max;
+        }
+
+        public bool isEqual(int a, int b, int c)
+        {
+            return a == b && a == c;
+        }
+
+        public int abs(int x)
+        {
+            if (x > 0)
+            {
+                return x;
+            }
+
+            else
+            {
+                return x * -1;
+            }
+        }
+
+        public bool is35(int x)
+        {
+            if (x % 3 == 0 && x % 5 == 0)
+            {
+                return false;
+            }
+            return x % 3 == 0 || x % 5 == 0;
+        }
+
+        public int max3(int x, int y, int z)
+        {
+            if (x > y && x > z) 
+            {
+                return x;
+            }
+
+            if (y > z && y > x)
+            {
+                return y;
+            }
+
+            else
+            {
+                return z;
+            }
+        }
+
+        public int sum2(int x, int y)
+        {
+            if ((x + y) < 20 && (x + y) > 9)
+            {
+                return 20;
+            }
+            return x + y;
+        }
+
+        public string day(int x)
+        {
+            string answer;
+            switch(x)
+            {
+                case 1: answer = "понедельник"; break;
+                
+                case 2: answer = "вторник"; break;
+                
+                case 3: answer = "среда"; break;
+                
+                case 4: answer = "четверг"; break;
+                
+                case 5: answer = "пятница"; break;
+                
+                case 6: answer = "суббота"; break;
+                
+                case 7: answer = "воскресение"; break;
+
+                default: answer = "день не найден"; break;
+            }
+
+            return answer;
+        }
+
+        public string listNums(int x)
+        {
+            string answer = "";
+            
+            for (int i = 0; i <= x; i++)
+            {
+                answer += Convert.ToString(i) + ' ';
+            }
+
+            return answer;
+        }
+
+        public String chet(int x)
+        {
+            string answer = "";
+
+            for (int i = 0; i <= x; i += 2)
+            {
+                answer += Convert.ToString(i) + ' ';
+            }
+
+            return answer;
+        }
+
+        public int numLen(long x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            
+            int answer = 0;
+            int d = 1;
+
+            while (x / d != 0)
+            {
+                answer++;
+                d *= 10;
+            }
+
+            return answer;
+        }
+
+        public void square(int x)
+        {
+            for (int _ = 0; _ < x; _++)
+            {
+                Console.WriteLine(new string('*', x));
+            }
+        }
+
+        public void rightTriangle(int x)
+        {
+            int emptys = x - 1;
+            int symbols = 1;
+            
+            for (int _ = 0; _ < x; _++)
+            {
+                Console.WriteLine(new string(' ', emptys) + new string('*', symbols));
+                symbols++;
+                emptys--;
+            }
+        }
+
+        public int findFirst(int[] arr, int x)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == x)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public int maxAbs(int[] arr)
+        {
+            int max = 0;
+            int ans = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                    ans = arr[i];
+                }
+
+                if (arr[i] * -1 > max)
+                {
+                    max = arr[i] * -1;
+                    ans = arr[i];
+                }
+            }
+
+            return ans;
+        }
+
+        public int[] add(int[] arr, int[] ins, int pos)
+        {
+            int[] result = new int[ins.Length + arr.Length];
+            int offset = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i == pos)
+                {
+                    for (int j = 0; j < ins.Length; j++)
+                    {
+                        result[i+j] = ins[j];
+                    }
+
+                    offset = ins.Length;
+                }
+
+                result[i + offset] = arr[i];
+            }
+
+            return result;
+        }
+
+        public int[] reverseBack(int[] arr)
+        {
+            int[] res_arr = new int[arr.Length];
+            int counter = 1;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                res_arr[i] = arr[arr.Length-counter];
+                counter++;
+            }
+
+            return res_arr;
+        }
+
+
+        public int[] findAll(int[] arr, int x)
+        {
+            int counter = 0;
+
+            for (int i = 0;i < arr.Length;i++)
+            {
+                if (arr[i] == x)
+                {  counter++; }
+            }
+
+            int[] res_arr = new int[counter];
+            int idx = 0;
+
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                if (arr[i] == x)
+                { 
+                    res_arr[idx] = i;
+                    idx++;
+                }
+            }
+
+            return res_arr;
+        }
+    }
+}
---
+++ lab 1/Program.cs
@@ -0,0 +1,209 @@
+﻿using cs_lab1_main;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Methods methods = new Methods();
+        Random random = new Random();
+
+        //1.1
+        Console.WriteLine("1.1 Введите вещественное x ");
+        double double_num = Convert.ToDouble(Console.ReadLine());    
+        Console.WriteLine(methods.fraction(double_num));
+
+        //1.3
+        Console.WriteLine("1.3 Введите символ (0-9) ");
+#pragma warning disable CS8604 // Возможно, аргумент-ссылка, допускающий значение NULL.
+        char symb = Convert.ToChar(Console.ReadLine());
+#pragma warning restore CS8604 // Возможно, аргумент-ссылка, допускающий значение NULL.
+        int answer = methods.charToNum(symb);
+        Console.WriteLine(answer.GetType().Name + ' ' +  answer);
+
+        //1.5
+        Console.WriteLine("1.5 Введите число x ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(x);
+        Console.WriteLine(methods.is2Digits(x));
+
+        //1.7
+        Console.WriteLine("1.7 Последовательно введите a, b и num");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(methods.isInRange(a, b, num));
+
+        //1.9
+        Console.WriteLine("1.9 Последовательно введите a, b и c");
+        a = Convert.ToInt32(Console.ReadLine());
+        b = Convert.ToInt32(Console.ReadLine());
+        int c = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(methods.isEqual(a, b, c));
+
+        //2.1
+        Console.WriteLine("2.1 Введите x");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(methods.abs(x));
+
+        //2.3
+        Console.WriteLine("2.3 Введите x");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(methods.is35(x));
+
+        //2.5
+        Console.WriteLine("2.5 Последовательно введите x, y и z ");
+        x = Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+        int z = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(methods.max3(x, y, z));
+
+        //2.7
+        Console.WriteLine("2.7 Последовательно введите x и y ");
+        x = Convert.ToInt32(Console.ReadLine());
+        y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(methods.sum2(x, y));
+
+        //2.9
+        Console.WriteLine("2.9 Введите номер дня недели: ");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(methods.day(x));
+
+        ////3.1
+        Console.WriteLine("3.1 Введите x ");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(methods.listNums(x));
+
+        //3.3
+        Console.WriteLine("3.3 Введите x ");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(methods.chet(x));
+
+        //3.5
+        Console.WriteLine("3.5 Введите x ");
+        long long_num = Convert.ToInt64(Console.ReadLine());
+        Console.WriteLine(methods.numLen(long_num));
+
+        //3.7
+        Console.WriteLine("3.7 Введите x ");
+        x = Convert.ToInt32(Console.ReadLine());
+        methods.square(x);
+
+        //3.9
+        Console.WriteLine("3.9 Введите x ");
+        x = Convert.ToInt32(Console.ReadLine());
+        methods.rightTriangle(x);
+
+        //4.1
+        Console.WriteLine("4.1 Введите длину массивa");
+        int length = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[length];
+        Console.WriteLine("Случайно сгенерированный массив");
+        
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = random.Next(1, 10);
+            Console.Write(arr[i]);
+            Console.Write(' ');
+        }
+        Console.WriteLine();
+        Console.WriteLine("Введите число x");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(methods.findFirst(arr, x));
+
+        //4.3
+        Console.WriteLine("4.3 Введите длину массивa");
+        length = Convert.ToInt32(Console.ReadLine());
+        int[] arr2 = new int[length];
+        Console.WriteLine("Случайно сгенерированный массив");
+
+        for (int i = 0; i < arr2.Length; i++)
+        {
+            arr2[i] = random.Next(-20, 20);
+            Console.Write(arr2[i]);
+            Console.Write(' ');
+        }
+
+        Console.WriteLine();
+        Console.WriteLine(methods.maxAbs(arr2));
+
+        //4.5
+        Console.WriteLine("4.5 Введите длину первого массива");
+        length = Convert.ToInt32(Console.ReadLine());
+        int[] arr3 = new int[length];
+        Console.WriteLine("Случайно сгенерированный первый массив");
+
+        for (int i = 0; i < arr3.Length; i++)
+        {
+            arr3[i] = random.Next(1, 10);
+            Console.Write(arr3[i]);
+            Console.Write(' ');
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Введите длинну второго массива");
+        length = Convert.ToInt32(Console.ReadLine());
+        int[] ins = new int[length];
+        Console.WriteLine("Случайно сгенерированный второй массив");
+
+        for (int i = 0; i < ins.Length; i++)
+        {
+            ins[i] = random.Next(1, 10);
+            Console.Write(ins[i]);
+            Console.Write(' ');
+        }
+        Console.WriteLine() ;
+
+        Console.WriteLine("Введите позицию первого массива в которую хотите вставить второй");
+        int pos = Convert.ToInt32(Console.ReadLine());
+        int[] answerArr =  methods.add(arr3, ins, pos);
+        for (int i = 0; i < answerArr.Length; i++)
+        {
+            Console.Write(answerArr[i]);
+            Console.Write(' ');
+        }
+        Console.WriteLine();
+
+        //4.7
+        Console.WriteLine("4.7 Введите длину массива");
+        length = Convert.ToInt32(Console.ReadLine());
+        int[] arr4 = new int[length];
+        Console.WriteLine("Случайно сгенерированный массив");
+
+        for (int i = 0; i < arr4.Length; i++)
+        {
+            arr4[i] = random.Next(1, 10);
+            Console.Write(arr4[i]);
+            Console.Write(' ');
+        }
+
+        Console.WriteLine();
+        int[] answerArr2 = methods.reverseBack(arr4);
+        Console.WriteLine("Перевернутый массив");
+        for (int i = 0;i < answerArr2.Length; i++) { Console.Write(answerArr2[i]); Console.Write(" "); }
+        Console.WriteLine();
+
+        //4.9
+        Console.WriteLine("4.9 Введите длину массива");
+        length = Convert.ToInt32(Console.ReadLine());
+        int[] arr5 = new int[length];
+
+        Console.WriteLine("Случайно сгенерированный массив");
+        for (int i = 0; i < arr5.Length; i++)
+        {
+            arr5[i] = random.Next(1,10);
+            Console.Write(arr5[i]);
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Введите x");
+        x = Convert.ToInt32(Console.ReadLine());
+        int[] answerArr3 = methods.findAll(arr5, x);
+        Console.WriteLine("Массив вхождений x в arr");
+        for (int i = 0; i < answerArr3.Length; i++)
+        {
+            Console.Write(answerArr3[i]);
+            Console.Write(" ");
+        }
+    }
+}
